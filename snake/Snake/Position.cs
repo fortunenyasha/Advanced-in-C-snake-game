87fc670a1356878211639cb49cf47e0958901869
Fortune Nyasha Mhirimo <139254193+fortunenyasha@users.noreply.github.com>
2023-10-04 16:07:29 +0200

    Add files via upload

---
+++ snake/Snake/Position.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public class Position
+    {
+        public int Row { get; }
+        public int Col { get; }
+        public Position(int row, int col)
+        {
+            Row = row;
+            Col = col;
+
+        }
+        public Position Translate(Direction dir)
+        {
+            return new Position(Row + dir.RowOffset, Col + dir.ColOffset);
+        }
+
+    }
+}
